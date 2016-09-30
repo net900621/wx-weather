6cad02a3ad63ae53b9d24316347b6553856bedaa
Yao Yao <yaoyao@egret.com>
2016-09-30 10:18:22 +0800

    yy

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,7 +13,7 @@ Page({
     var that = this
 
     wx.request({
-      'url': 'http://www.weather.com.cn/data/cityinfo/' + citys[this.data.city] + '.html'
+      'url': 'http://www.weather.com.cn/data/cityinfo/' + citys[this.data.city.replace(/市|县|区|镇|省/g, '')] + '.html'
       ,'method': 'GET'
       ,'success': function(data){
         if (data.statusCode == 200 && /ok/.test(data.errMsg)) {
@@ -25,5 +25,31 @@ Page({
       }
     })
 
+  },
+  onReady: function(){
+    var that = this;
+    wx.getLocation({
+      'success': function(e){
+        wx.request({
+          'url': 'http://api.map.baidu.com/geocoder/v2/',
+          'data': {
+            'ak': '95E5007f38fb52241ee04113c6e97501',
+            'callback': 'renderReverse',
+            'location': e.latitude + ',' + e.longitude,
+            'output': 'json',
+            'pois': 1
+          },
+          'success': function(data){
+            if (data.statusCode == 200 && /ok/.test(data.errMsg)) {
+              data = JSON.parse(data.data.replace(/^renderReverse\&\&renderReverse\(|\)$/g, ''));
+              that.setData({
+                city: data.result.addressComponent.city
+              });
+              that.searchWeathes();
+            }
+          }
+        })
+      }
+    })
   }
 })
